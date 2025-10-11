4d29090e3c446270de67424e8a0e4952611ae407
Joey-Li0118 <joeyli2797@gmail.com>
2025-10-11 11:21:19 -0500

    finished exercises 3.1-3.6, phonebook without react

--- part3/A/index.js
+++ part3/A/index.js
@@ -1,51 +1,67 @@
 const express = require('express')
 const app = express()
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
 ]
 
 
 app.use(express.json())
 
-app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
-  response.json(note)
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+   if (!(person.name && person.number)) {
+    return response.status(400).json({error: "content missing"});
+  }
+  const existingPerson = persons.find(people => people.name === person.name);
+  const id = Math.random(0, 100000)
+  if (existingPerson) {
+    return response.status(400).json({error: "Person already exists"});
+  } 
+    person.id = id
+    persons.push(person)
+    response.json(person)
+// The name or number is missing
+// The name already exists in the phonebook
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  response.json(note)
-  if (note) { // if note exists then ok otherwise either undefined or null 
-    response.json(note)
+  const person = persons.find(person => person.id === id)
+  response.json(person)
+  if (person) { // if note exists then ok otherwise either undefined or null 
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
