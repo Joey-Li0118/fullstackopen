5d1c26a7744a45697b67d0b1578eefbfa273e719
Joey-Li0118 <joeyli2797@gmail.com>
2025-11-07 10:15:47 -0600

    deployment

--- part3/A/index.js
+++ part3/A/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 app.use(express.json())
-
+app.use(cors())
 
 let persons = [
     { 
@@ -74,8 +75,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-  
 })
